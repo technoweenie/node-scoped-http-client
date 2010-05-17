3a82e8d0d398f0002fb1c62e80d24152186be757
rick <technoweenie@gmail.com>
2010-05-16 21:58:22 -0700

    add client support for sending headers and params

--- test/scoped_get_test.js
+++ test/scoped_get_test.js
@@ -1,24 +1,31 @@
 var ScopedClient = require('../lib')
+  ,          sys = require('sys')
   ,         http = require('http')
   ,       assert = require('assert')
-  ,       called = false
+  ,       called = 0
   ,       client
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'})
-  res.end('hello')
+  res.end(req.method + ' ' + req.url + ' -- hello ' + req.headers['accept'])
 })
 server.listen(9999)
 
-client = ScopedClient.create('http://localhost:9999')
+client = ScopedClient.create('http://localhost:9999', {headers: {accept: 'text/plain'}})
 client.get()(function(resp, body) {
-  called = true
+  called++
   assert.equal(200,          resp.statusCode)
   assert.equal('text/plain', resp.headers['content-type'])
-  assert.equal('hello',      body)
-  server.close()
+  assert.equal('GET / -- hello text/plain', body)
+  client.path('/a').query('b', '1').get()(function(resp, body) {
+    called++
+    assert.equal(200,          resp.statusCode)
+    assert.equal('text/plain', resp.headers['content-type'])
+    assert.equal('GET /a?b=1 -- hello text/plain', body)
+    server.close()
+  })
 })
 
 process.addListener('exit', function() {
-  assert.ok(called)
+  assert.equal(2, called)
 })
